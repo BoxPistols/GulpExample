68e976f84f2c4c5d28742bce99b151e8298d86ab
ait <ait@ex.com>
2019-07-02 05:46:44 +0900

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
-// gulpプラグインの読み込み
 const gulp = require("gulp");
-// Sassをコンパイルするプラグインの読み込み
 const sass = require("gulp-sass");
 
-// style.scssをタスクを作成する
 gulp.task("default", function() {
-  // style.scssファイルを取得
-  return (
-    gulp
-      .src("css/style.scss")
-      // Sassのコンパイルを実行
-      .pipe(sass())
-      // cssフォルダー以下に保存
-      .pipe(gulp.dest("css"))
-  );
-});
+  return gulp.watch("css/**/*.sass", function() {
 
+    return (
+      gulp
+      .src("css/**/*.sass")
+      .pipe(
+        sass({
+          outputStyle: "expanded"
+        })
+        .on("error", sass.logError)
+      )
+      .pipe(gulp.dest("css"))
+    );
+  });
+});
